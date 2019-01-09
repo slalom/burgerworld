2e170c4b3d1915413b2e96ca6313bb6f4a44d780
Duy Le <dlee@russellinvestments.com>
2019-01-09 10:10:03 -0800

    update script to correct items table

--- db_src/CreateCassandraDB.sql
+++ db_src/CreateCassandraDB.sql
@@ -6,13 +6,13 @@ CREATE KEYSPACE IF NOT EXISTS burger_world
   
   DROP TABLE IF EXISTS burger_world.items;
   
-  CREATE TABLE IF NOT EXISTS burger_world.items (
+  CREATE TABLE IF NOT EXISTS burger_world.item (
    id text PRIMARY KEY, 
    name text, 
    price double
   );
   
 /*
-  INSERT INTO burger_world.items (id, name, price) VALUES ('nate id', 'nate', 1000000);
-  SELECT * FROM burger_world.items;
+  INSERT INTO burger_world.item (id, name, price) VALUES ('nameid', 'name', 1980000);
+  SELECT * FROM burger_world.item;
 */
