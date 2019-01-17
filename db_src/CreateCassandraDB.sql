173497f23d2c755b959dcc5fec1c44aeff9923ff
Peter <peter.luangrath@slalom.com>
2019-01-17 14:49:10 -0800

    added new column into cassandra

--- db_src/CreateCassandraDB.sql
+++ db_src/CreateCassandraDB.sql
@@ -11,7 +11,8 @@ CREATE KEYSPACE IF NOT EXISTS burger_world
   CREATE TABLE IF NOT EXISTS burger_world.item (
    id text PRIMARY KEY, 
    name text, 
-   price double
+   price double,
+   imageFileName text
   );
   
     CREATE TABLE IF NOT EXISTS burger_world.productorder (
@@ -31,7 +32,10 @@ CREATE KEYSPACE IF NOT EXISTS burger_world
   
     INSERT INTO burger_world.productorder (id, poNumber, customerName, orderDate) VALUES ('order123', 'po1234', 'Nathaniel Davis Jr.', '2019-01-19');
   
-  INSERT INTO burger_world.item (id, name, price) VALUES ('nameid', 'name', 1980000);
+  INSERT INTO burger_world.item (id, name, price, imagefilename) VALUES ('id1', 'Burger 1', 7.99, 'burger-1.png');
+   INSERT INTO burger_world.item (id, name, price, imagefilename) VALUES ('id2', 'Soda Pop 1', 2.99, 'soda-pop-1.png');
+   INSERT INTO burger_world.item (id, name, price, imagefilename) VALUES ('id3', 'French Fries 1', 4.99, 'french-fries-1.jpg');
+  
   SELECT * FROM burger_world.item;
   
   
